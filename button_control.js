a682bebfc153eb29c4aed87d235d174eb8001132
Guthrie Schoolar <guthrieschoolar95@gmail.com>
2017-01-04 01:13:57 -0600

    Added Button Functionality
    The time can be set by the various '+' and '-' signs below the
    respective time slots. The time can also be cleared using the 'clear'
    button.

--- button_control.js
+++ button_control.js
@@ -2,6 +2,7 @@
 /*global $, jQuery, alert*/
 'use strict';
 
+/*hover*/
 $("#hours_up").hover(function () {
 	$("#hour_num").animate({"color": "#5DA9E9"}, 150);
 }, function () {
@@ -36,4 +37,63 @@ $("#sec_down").hover(function () {
 	$("#sec_num").animate({"color": "#982649"}, 150);
 }, function () {
 	$("#sec_num").animate({"color": "#011627"}, 150);
+});
+
+
+/*manipulate time on popup*/
+var num = $("#hour_num").text();
+$("#hours_up").click(function () {
+	num = Number(num);
+	num = num + 1;
+	$("#hour_num").text(num >= 10 ? String(num) : '0' + String(num));
+});
+$("#hours_down").click(function () {
+	num = $("#hour_num").text();
+	num = Number(num);
+	num = num - 1;
+	if (num <= 0) {
+		num = 0;
+	}
+	$("#hour_num").text(num >= 10 ? String(num) : '0' + String(num));
+});
+
+$("#min_up").click(function () {
+	num = $("#min_num").text();
+	num = Number(num);
+	num = num + 10;
+	$("#min_num").text(num >= 10 ? String(num) : '0' + String(num));
+});
+$("#min_down").click(function () {
+	num = $("#min_num").text();
+	num = Number(num);
+	num = num - 10;
+	if (num <= 0) {
+		num = 0;
+	}
+	$("#min_num").text(num >= 10 ? String(num) : '0' + String(num));
+});
+
+$("#sec_up").click(function () {
+	num = $("#sec_num").text();
+	num = Number(num);
+	num = num + 10;
+	$("#sec_num").text(num >= 10 ? String(num) : '0' + String(num));
+});
+$("#sec_down").click(function () {
+	num = $("#sec_num").text();
+	num = Number(num);
+	num = num - 10;
+	if (num <= 0) {
+		num = 0;
+	}
+	$("#sec_num").text(num >= 10 ? String(num) : '0' + String(num));
+});
+
+
+/*clear the timer on click*/
+$("#clear").click(function () {
+	$("#hour_num").text('00');
+	$("#min_num").text('00');
+	$("#sec_num").text('00');
+	num = 0;
 });
