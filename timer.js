e201ad7ebdac427c1d829ce6b90a373b038fe6d5
Guthrie Schoolar <guthrieschoolar95@gmail.com>
2017-01-05 00:15:36 -0600

    added bit o' polish
    Added some more animations so that when the timer begins, the buttons
    that manipulate it go away until the timer is up.

--- timer.js
+++ timer.js
@@ -5,6 +5,19 @@
 /*mode: 0 = not running, 1 = running*/
 var mode = 0;
 var interval = 0;
+
+function animations() {
+	$(".controls").animate({
+		height: 'toggle'
+	}, 500);
+	$("#begin").animate({
+		height: 'toggle'
+	});
+	$("#clear").animate({
+		height: 'toggle'
+	});
+}
+
 function getTime() {
 	/*convert each time string into seconds*/
 	var h = Number($("#hour_num").text()) * 3600,
@@ -20,16 +33,19 @@ function timer() {
 		/*update the numbers on the screen*/
 		$("#hour_num").text(('0' + String(Math.floor(t / 3600))).slice(-2));
 		$("#min_num").text(('0' + String(Math.floor(t / 60))).slice(-2));
-		$("#sec_num").text(Math.floor(t % 60));
+		$("#sec_num").text(('0' + String(Math.floor(t % 60))).slice(-2));
 	} else {
 		mode = 0;
 		clearInterval(interval);
+		interval = 0;
+		animations();
 	}
 }
 
 /*activate on begin button click*/
 $("#begin").click(function () {
 	if (mode === 0) {
+		animations();
 		mode = 1; /*clock is running*/
 		interval = setInterval(timer, 1000);
 	}
